ddedc5bf8dd23b7a0925ad97f34a941191aa6b46
jagadeesh93461 <konetijagadeesh7@gmail.com>
2024-11-26 21:55:12 +0530

    added

--- application.java
+++ application.java
@@ -4,3 +4,25 @@ public class Main {
     System.out.println(myNum);
   }
 }
+#feature201 changed by dev1
+public class Main {
+  public static void main(String[] args) {
+    int x = 5, y = 6, z = 50;
+    System.out.println(x + y + z);
+  }
+#end feature201
+}
+feature202 changed by dev2
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+  }
+# end feature202
+}
