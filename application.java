5f3681e9080827787f9c3809e5c48d0906d97f2c
jagadeesh93461 <konetijagadeesh7@gmail.com>
2024-11-26 21:34:25 +0530

    application.java

--- application.java
+++ application.java
@@ -4,3 +4,10 @@ public class Main {
     System.out.println(myNum);
   }
 }
+public class Main {
+  public static void main(String[] args) {
+    int x = 5, y = 6, z = 50;
+    System.out.println(x + y + z);
+  }
+#feature201 changed by dev1
+}
